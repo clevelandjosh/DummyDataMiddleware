55da0248067ec39f6baf2d280f9056530d9774a1
clevelandjosh <33519616+clevelandjosh@users.noreply.github.com>
2017-11-29 14:18:27 -0500

    updated directory_and_file_creation.sh

--- directory_and_file_creation.sh
+++ directory_and_file_creation.sh
@@ -20,3 +20,20 @@ chmod 777 /tmp/alice/*/ww_file_with*
 chmod 755 /tmp/alice/*/not_ww_file_with*
 chmod +t /tmp/alice/*/*file_with_sticky
 
+cd /tmp
+mkdir /tmp/root
+cd /tmp/root
+mkdir  not_ww_dir_without_sticky  not_ww_dir_with_sticky  ww_dir_without_sticky  ww_dir_with_sticky
+  for i in $(ls -d */)
+   do
+    touch $i/not_ww_file_without_sticky
+    touch $i/not_ww_file_with_sticky
+    touch $i/ww_file_without_sticky
+    touch $i/ww_file_with_sticky
+  done
+chmod 755 /tmp/root/not_ww_dir_with*
+chmod 777 /tmp/root/ww_dir_with*
+chmod +t /tmp/root/*dir_with_sticky
+chmod 777 /tmp/root/*/ww_file_with*
+chmod 755 /tmp/root/*/not_ww_file_with*
+chmod +t /tmp/root/*/*file_with_sticky
