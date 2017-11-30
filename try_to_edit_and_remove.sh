89c13c479617daa41ccb29948c2203a72f60ab1a
clevelandjosh <33519616+clevelandjosh@users.noreply.github.com>
2017-11-30 10:54:41 -0500

    Update try_to_edit_and_remove.sh

--- try_to_edit_and_remove.sh
+++ try_to_edit_and_remove.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+# This should be run as bob. Switch user to bob before execution.
 cd /tmp/alice
 for i in $(ls -d */);
   do
   cd $i
     for j in `ls`
       do
-rm *.new
-chmod u+s $j
  /bin/cat <<EOM >$j
 #!/bin/bash
-touch $j.new
-chmod 777 $j.new
-chmod u+s $j.new
+touch $j.new &> /dev/null
+chmod 777 $j.new &> /dev/null
+chmod u+s $j.new &> /dev/null
 EOM
    done
 
@@ -25,17 +24,16 @@ for i in $(ls -d */);
   cd $i
     for j in `ls`
       do
-rm *.new
-chmod u+s $j
  /bin/cat <<EOM >$j
 #!/bin/bash
-touch $j.new
-chmod 777 $j.new
-chmod u+s $j.new
+touch $j.new &> /dev/null
+chmod 777 $j.new &> /dev/null
+chmod u+s $j.new &> /dev/null
 EOM
    done
 
 cd ..
 done
 cd /tmp
+ls -al /tmp/alice/*/* /tmp/root/*/* > initial_results.txt
 exit
